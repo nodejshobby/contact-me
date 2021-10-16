7cef36b37428b4c90f0cb7412951ac5c10034375
nodejshobby@gmail.com <nodejshobby@gmail.com>
2021-10-16 19:25:49 +0100

    Fixing javascript frontend error

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
 
 app.post('/contact', validateContact, (req, res) => {
   const errors = validationResult(req)
+
   if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
 
   const { subject, name, email, message } = req.body
